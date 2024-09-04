bce4a41a3b162944510115421dde73c4ea568083
Juan Patricio <ggjuanpatricio@gmail.com>
2024-09-04 05:18:08 -0300

    hg

--- chatbot.js
+++ chatbot.js
@@ -99,10 +99,12 @@ const handleIncomingMessage = async (msg, { provider, flowDynamic, fallBack }) =
         await flowDynamic(["lo siento, aun no puedo entender imagenes :( "])
         return fallBack("");
     } else {
-        async ()=>{
+        
+        const response = async ()=>{
             console.log("enviando...")
             await flowDynamic([{ body: " ", media: "/root/chat-kapselmaker/img.jpg" }]);
         }
+        response()
         await updateContext(from, 'Usuario', text);
         const context = await getContext(from)
         const finalResponse = await iaAgent(context, API_URL, text);
